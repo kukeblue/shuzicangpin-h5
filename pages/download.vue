5e9f168e053e41f56f51e7281a36a767c59ef75e
GOGOHQ <657473650@qq.com>
2022-05-06 15:35:33 +0800

    @

---
+++ pages/download.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="download-page">
+        <div class="content flex-column-center">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: "noLoginLayout", // you can set a custom layout for the error page
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
+
+<style lang='scss'>
+.download-page {
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+
+    .content {
+        margin: 2em, 1em, 1.5em, 1em
+    }
+}
+</style>
